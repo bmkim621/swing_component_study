31f1ce88bdb13bf35e02255e4b0a1275ac570bac
bmkim621 <moonstruck0614@gmail.com>
2018-11-08 15:35:37 +0900

    JSliderChangeEventEx.java 파일 수정

--- src/swing_component_study/jcomponent/JSliderChangeEventEx.java
+++ src/swing_component_study/jcomponent/JSliderChangeEventEx.java
@@ -1,23 +1,28 @@
 package swing_component_study.jcomponent;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import javax.swing.border.TitledBorder;
 import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SpinnerListModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.event.ChangeListener;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
-import java.awt.Color;
+import javax.swing.event.ChangeListener;
 
 public class JSliderChangeEventEx extends JFrame implements ActionListener, ChangeListener {
 
@@ -29,6 +34,12 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 	private JSlider sliderG;
 	private JSlider sliderB;
 	private JSlider sliderR;
+	private JPanel pSpinner;
+	private JSpinner spList;
+	private JSpinner spNumber;
+	private JSpinner spDate;
+	private JButton btnNewButton;
+	private JPanel panel;
 
 
 	/**
@@ -41,11 +52,11 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 		setForeground(Color.RED);
 		setTitle("JSliderChangeEventEx");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 638, 408);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 2, 10, 0));
+		contentPane.setLayout(new GridLayout(0, 3, 10, 0));
 		
 		JPanel pBasicSlider = new JPanel();
 		pBasicSlider.setBorder(new TitledBorder(null, "\uC2AC\uB77C\uC774\uB354 \uCEF4\uD3EC\uB10C\uD2B8", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -106,6 +117,7 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 		sliderB.setPaintLabels(true);
 		pAdvanceSlider.add(sliderB);
 		
+		
 		lblColor = new JLabel("SLIDER EXAMPLE");
 		lblColor.setOpaque(true);
 		lblColor.setBackground(new Color(sliderR.getValue(), sliderG.getValue(), sliderB.getValue()));
@@ -116,9 +128,46 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 		sliderR.addChangeListener(this);
 		sliderG.addChangeListener(this);
 		sliderB.addChangeListener(this);
+		
+		pSpinner = new JPanel();
+		contentPane.add(pSpinner);
+		pSpinner.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		panel = new JPanel();
+		pSpinner.add(panel);
+		
+		spList = new JSpinner();
+		spList.setModel(new SpinnerListModel(new String[] {"\uC18C\uC124", "\uC7A1\uC9C0", "\uC804\uACF5\uC11C\uC801", "\uCDE8\uBBF8"}));
+		panel.add(spList);
+		
+		spNumber = new JSpinner();
+		spNumber.setModel(new SpinnerNumberModel(1, 0, 9, 1));
+		panel.add(spNumber);
+		
+		spDate = new JSpinner();
+		Calendar calendar = Calendar.getInstance();
+		Date value = calendar.getTime();
+		
+		calendar.add(Calendar.YEAR, -50);
+		Date start = calendar.getTime();
+		
+		calendar.add(calendar.YEAR, 100);
+		Date end = calendar.getTime();
+		
+		spDate.setModel(new SpinnerDateModel(new Date(1541602800000L), start, end, Calendar.YEAR));
+		spDate.setEditor(new JSpinner.DateEditor(spDate, "yyyy/MM/dd"));
+		
+		panel.add(spDate);
+		
+		btnNewButton = new JButton("값 가져오기");
+		btnNewButton.addActionListener(this);
+		pSpinner.add(btnNewButton);
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNewButton) {
+			do_btnNewButton_actionPerformed(e);
+		}
 		if (e.getSource() == btnOk) {
 			do_btnOk_actionPerformed(e);
 		}
@@ -133,7 +182,10 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 	
 	public void stateChanged(ChangeEvent e) {
 		if (e.getSource() == sliderR || e.getSource() == sliderG || e.getSource() == sliderB) {
-			lblColor.setBackground(new Color(sliderR.getValue(), sliderG.getValue(), sliderB.getValue()));
+			Color color = new Color(sliderR.getValue(), sliderG.getValue(), sliderB.getValue());
+			lblColor.setBackground(color);
+			Color fcolor = new Color(255-sliderR.getValue(), 255-sliderG.getValue(), 255-sliderB.getValue());
+			lblColor.setForeground(fcolor);
 		}
 	}
 	
@@ -141,5 +193,18 @@ public class JSliderChangeEventEx extends JFrame implements ActionListener, Chan
 		//setText는 String이므로 int오면 X, int오면 뒤에 "" 붙여서 문자로
 		int value = slider.getValue();
 		lblValue.setText(value + "");
+	}
+	
+	//스피너 값 가져오기
+	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+		//리스트
+		String getItem = (String) spList.getValue();
+		//숫자
+		int getNum = (int) spNumber.getValue();
+		//날짜
+		Date getDate = (Date) spDate.getValue();
+		//날짜 포맷
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		JOptionPane.showMessageDialog(null, "item : " + getItem + "\nNumber : " + getNum + "\nDate : " + sdf.format(getDate));
 	}
 }
